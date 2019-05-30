7972bd1841c3609a16b6582789d41c4f9ca45f10
邱红理 <48619949+Latepen@users.noreply.github.com>
2019-05-30 14:07:16 +0800

    Python File

---
+++ qiuhongli.py
@@ -0,0 +1,14 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/Admin/Desktop/qiuhongli.py ================
+█Getting uuid of QR code.
+Downloading QR code.
+Please scan the QR code to log in.
+Please press confirm on your phone.
+Loading the contact, this may take a little while.
+Login successfully as 迟笔
+男性好友：71.82%
+女性好友：17.27%
+其他：11.82%
+>>> LOG OUT!
